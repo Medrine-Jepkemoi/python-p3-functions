e2a0196ec494d8089ef47439ae12ba076585b910
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-20 23:38:28 +0300

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Medrine")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("John Doe")
 
 def add(num1, num2):
-    pass
+    answer = num1 + num2
+    return answer
+
+print(add(3, 4))    
 
 def halve(number):
-    pass
+    half = number / 2
+    return half
+
+print(halve(5))
